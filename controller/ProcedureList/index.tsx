dbff4df7ff362ca5a4f7fde0e365a28e1488291f
Manuel Ruck <github@manuelruck.de>
2018-10-22 10:51:58 +0200

    prettify

--- controller/ProcedureList/index.tsx
+++ controller/ProcedureList/index.tsx
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
+import http from 'http';
 import * as React from 'react';
 import { Query } from 'react-apollo';
 import { List } from 'semantic-ui-react';
-import http from 'http';
 
 import { ProceduresList, ProceduresList_procedures } from '../schemaTypes';
 
@@ -26,23 +26,36 @@ interface IProps {
   listTypes: any;
 }
 
-const postHC = ({procedureId} : {procedureId:string}) => () => {
-  console.log({procedureId});
+const postHC = ({ procedureId }: { procedureId: string }) => () => {
+  console.log({ procedureId });
 
-  http.get(`http://localhost:3000/webhooks/human-connection/contribute?procedureId=${procedureId}`, (resp) => {
-    console.log(resp);
-  });
-}
+  http.get(
+    `http://localhost:3000/webhooks/human-connection/contribute?procedureId=${procedureId}`,
+    resp => {
+      console.log(resp);
+    },
+  );
+};
 
 class ProcedureListQuery extends Query<ProceduresList> {}
 
 export const ProcedureList: React.SFC<IProps> = props => (
   <List>
-    <ProcedureListQuery query={proceduresListQuery} variables={{listTypes: props.listTypes}}>
+    <ProcedureListQuery
+      query={proceduresListQuery}
+      variables={{ listTypes: props.listTypes }}
+    >
       {({ data }: IProcedureListProps) => {
-        return (!data || !data.procedures) ? null : data.procedures.map(({ title, procedureId }) => {
-          return <List.Item key={procedureId}><p>{title}</p><button onClick={postHC({procedureId})}>Post to HC</button></List.Item>;
-        });
+        return !data || !data.procedures
+          ? null
+          : data.procedures.map(({ title, procedureId }) => {
+              return (
+                <List.Item key={procedureId}>
+                  <p>{title}</p>
+                  <button onClick={postHC({ procedureId })}>Post to HC</button>
+                </List.Item>
+              );
+            });
       }}
     </ProcedureListQuery>
   </List>
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const basicAuth = require("basic-auth-connect");
+const express = require('express');
+const basicAuth = require('basic-auth-connect');
 import * as next from 'next';
 
 const PORT = Number(process.env.PORT) || 3003;
@@ -7,22 +7,30 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  if (!dev) {
-    server.use(basicAuth(process.env.ADMIN_USER || 'root', process.env.ADMIN_PASSWORD || 'root'));
-  }
+    if (!dev) {
+      server.use(
+        basicAuth(
+          process.env.ADMIN_USER || 'root',
+          process.env.ADMIN_PASSWORD || 'root',
+        ),
+      );
+    }
 
-  server.get("*", (req:any, res:any) => {
-    return handle(req, res);
+    server.get('*', (req: any, res: any) => {
+      return handle(req, res);
+    });
+
+    server.listen(PORT, (err: any) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-
-  server.listen(PORT, (err:any) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-}).catch(ex => {
-  console.error(ex.stack);
-  process.exit(1);
-});
